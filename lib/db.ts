c3b07d8ce61a046a4b5ee9f31d3a4f595f64481b
locifer18 <anshkanda08@gmail.com>
2026-04-29 12:27:30 +0530

    added

--- lib/db.ts
+++ lib/db.ts
@@ -10,6 +10,7 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 
+
 /**
  * Create Prisma client with proper configuration
  * Reuses instance in development to avoid "too many connections" errors
